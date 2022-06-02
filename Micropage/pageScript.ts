22d518b7cfa2af267efc7395add801337aba5948
Lisawa23 <lisa.maria.waletzko@hs-furtwangen.de>
2022-06-02 12:25:25 +0200

    andere Seiten gefüllt

--- Micropage/pageScript.ts
+++ Micropage/pageScript.ts
@@ -1,43 +1,78 @@
 
 
 window.addEventListener("load", function(): void {
-let text: HTMLElement = document.querySelector(".text");
 let hideBlock: HTMLElement = document.querySelector(".timeline-panel");
 document.querySelector(".mfirst").addEventListener("mouseover", function(): void {
+  let text: HTMLElement = document.querySelector(".text1");
+  let text2: HTMLElement = document.querySelector(".text2");
+  let text3: HTMLElement = document.querySelector(".text3");
+  let text4: HTMLElement = document.querySelector(".text4");
   hideBlock.classList.remove("hidden");
-  text.innerHTML = "...";
+  text.innerHTML = "Vorlesung";
+  text2.innerHTML = "";
+  text3.innerHTML = "";
+  text4.innerHTML = "";
 });
 document.querySelector(".mfirst").addEventListener("mouseout", function(): void {
   hideBlock.classList.add("hidden");
 });
 document.querySelector(".m2").addEventListener("mouseover", function(): void {
+  let text: HTMLElement = document.querySelector(".text1");
+  let text2: HTMLElement = document.querySelector(".text2");
+  let text3: HTMLElement = document.querySelector(".text3");
+  let text4: HTMLElement = document.querySelector(".text4");
   hideBlock.classList.remove("hidden");
-  text.innerHTML = "...1";
+  text.innerHTML = "Ideenfindung";
+  text2.innerHTML = "Pitchentwurf 1";
+  text3.innerHTML = "Pitch 1";
+  text4.innerHTML = "";
 });
 document.querySelector(".m2").addEventListener("mouseout", function(): void {
   hideBlock.classList.add("hidden");
 });
 document.querySelector(".m3").addEventListener("mouseover", function(): void {
+  let text: HTMLElement = document.querySelector(".text1");
+  let text2: HTMLElement = document.querySelector(".text2");
+  let text3: HTMLElement = document.querySelector(".text3");
+  let text4: HTMLElement = document.querySelector(".text4");
   hideBlock.classList.remove("hidden");
-  text.innerHTML = "...2";
+  text.innerHTML = "Pitchentwicklung";
+  text2.innerHTML = "Pitch 2";
+  text3.innerHTML = "Konzeptentwicklung";
+  text4.innerHTML = "Illustrationen erstellen";
 });
 document.querySelector(".m3").addEventListener("mouseout", function(): void {
   hideBlock.classList.add("hidden");
 });
 document.querySelector(".m4").addEventListener("mouseover", function(): void {
+  let text: HTMLElement = document.querySelector(".text1");
+  let text2: HTMLElement = document.querySelector(".text2");
+  let text3: HTMLElement = document.querySelector(".text3");
+  let text4: HTMLElement = document.querySelector(".text4");
   hideBlock.classList.remove("hidden");
-  text.innerHTML = "...3";
+  text.innerHTML = "Animieren";
+  text2.innerHTML = "Postproduktion";
+  text3.innerHTML = "Präsentationsvorbereitung";
+  text4.innerHTML = "Präsentieren";
 });
 document.querySelector(".m4").addEventListener("mouseout", function(): void {
   hideBlock.classList.add("hidden");
 });
 document.querySelector(".mlast").addEventListener("mouseover", function(): void {
+  let text: HTMLElement = document.querySelector(".text1");
+  let text2: HTMLElement = document.querySelector(".text2");
+  let text3: HTMLElement = document.querySelector(".text3");
+  let text4: HTMLElement = document.querySelector(".text4");
   hideBlock.classList.remove("hidden");
-  text.innerHTML = "...4";
+  text.innerHTML = "Letzte Schliffe";
+  text2.innerHTML = "Micropage erstellen";
+  text3.innerHTML = "";
+  text4.innerHTML = "";
 });
 document.querySelector(".mlast").addEventListener("mouseout", function(): void {
   hideBlock.classList.add("hidden");
 });
+
 
 
 });
